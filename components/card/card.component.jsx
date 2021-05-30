c036a5665e9c2ebcbfc148b98659acd5748af52a
Igor Procópio <procopio.igor@hotmail.com>
2021-05-30 19:18:35 -0300

    loop temas

--- components/card/card.component.jsx
+++ components/card/card.component.jsx
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
-import {StyleSheet, View } from 'react-native';
-import {Button, Card, Paragraph, ProgressBar, Colors } from 'react-native-paper';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button, Card, Paragraph, ProgressBar, Colors } from 'react-native-paper';
 
 
 const styles = StyleSheet.create({
     card: {
-        backgroundColor: "#F2CCD5",
+        backgroundColor: "#F8E7E2",
         margin: 12,
         borderRadius: 20,
         flex: 1,
@@ -19,53 +19,54 @@ const styles = StyleSheet.create({
     cover: {
         height: 220,
     },
-    button:{
-    alignItems: "center",
-    flex: 1,
-    borderRadius: 200,
-    fontSize: 72
+    button: {
+        alignItems: "center",
+        flex: 1,
+        borderRadius: 200,
+        fontSize: 72
 
-    },   
-    text:{
+    },
+    text: {
         textAlign: "center",
         flex: 1,
-        
         margin: 12,
         fontWeight: 'bold'
     },
-    barra:{
+    barra: {
         height: 12,
         width: '100%',
-        backgroundColor: '#9C1837',
-        borderRadius: 20,
+        backgroundColor: '#925154',
+        borderRadius: 5,
 
     }
 });
 
 
 
-const CardQuote = ({task, background, theme, theme2}) => {
+const CardQuote = ({ task, background, theme, theme2 }) => {
 
-const [getPlay, setPlay] = useState('play');
+    const [getPlay, setPlay] = useState('play');
     const alterarEstado = () => {
-        if(getPlay == "play"){
-            setPlay ("pause")
-            
-           
+        if (getPlay == "play") {
+            setPlay("pause")
+
+
         } else {
-            setPlay ('play')
+            setPlay('play')
         }
     }
-        
-   const [getTema, setTema] = useState(background);
-        const alterarTema = () => {
-            if (getTema == background){
-                setTema(theme)
-            }else{
-             (getTema == theme)
+
+    const [getTema, setTema] = useState(background);
+    const alterarTema = () => {
+        if (getTema == background) {
+            setTema(theme)
+        } else {
+            if (getTema == theme) {
                 setTema(theme2)
-            }
-        } 
+            } else
+                setTema(background)
+        }
+    }
 
     return (
         <Card style={styles.card}>
@@ -75,29 +76,27 @@ const [getPlay, setPlay] = useState('play');
                 source={getTema}>
             </Card.Cover>
             <Button
-                    style = {styles.button}
-                    icon = "share"
-                    color = "#F2CCD5"
-                    labelStyle={{fontSize: 72}}
-                    borderRadius = "0"
-                    mode="contained"
-                    onPress={alterarTema}>    
+                style={styles.button}
+                icon="share"
+                color="#F8E7E2"
+                labelStyle={{ fontSize: 72 }}
+                mode="contained"
+                onPress={alterarTema}>
             </Button>
             <Card.Content style={styles.content}>
             </Card.Content>
-            <Card.Actions> 
-            <Button
-                    style = {styles.button}
-                    icon = {getPlay}
-                    color = "#F2CCD5"
-                    labelStyle={{fontSize: 72}}
-                    borderRadius = "0"
+            <Card.Actions>
+                <Button
+                    style={styles.button}
+                    icon={getPlay}
+                    color="#F8E7E2"
+                    labelStyle={{ fontSize: 72 }}
                     mode="contained"
-                    onPress={alterarEstado}>    
-            </Button>
+                    onPress={alterarEstado}>
+                </Button>
             </Card.Actions>
-            <Paragraph style= {styles.text}> {task} </Paragraph>
-            <View style = {styles.barra} ></View>
+            <Paragraph style={styles.text}> {task} </Paragraph>
+            <View style={styles.barra} ></View>
         </Card>
     )
 }
